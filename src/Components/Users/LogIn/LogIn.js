5bda0eaa560588d1848fa76fd06ac55b2f2442c8
sergeyivanchik <ivanchik-s@mail.ru>
2019-03-20 18:54:27 +0300

    add src Users

---
+++ src/Components/Users/LogIn/LogIn.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './LogIn.scss'
+
+
+
+class LogIn extends React.Component {
+  render() {
+    return (
+      <div className = "login-form">
+        <form method = "get">
+          <label className = "login-form__title">Log in</label>
+          <input type = "text" className = "login-form__username" placeholder = "username"/>
+          <input type = "password" className = "login-form__password" placeholder = "password"/>
+          <Link to="/">
+            <input type = "submit" className = "login-form__button" value = "Log in"/>
+          </Link>
+          <Link to="/signin">
+            <span className = "login-form__signup" >Sign in</span>
+          </Link>
+        </form>
+      </div>  
+    )
+  }
+}
+
+export default LogIn
---
+++ src/Components/Users/SignIn/SignIn.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './SignIn.scss'
+
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div className = "signin-form">
+        <form method = "get">
+          <label className = "signin-form__title">Sign in</label>
+          <input type = "text" className = "signin-form__name" placeholder = "name" required/>
+          <input type = "text" className = "signin-form__name" placeholder = "login" required/>
+          <input type = "text" className = "signin-form__password" placeholder = "password" required/>
+          <input type = "text" className = "signin-form__email" placeholder = "email" required/>
+          <Link to="/login">
+            <input type = "submit" className = "signin-form__button" value = "Sign in"/>
+          </Link>
+        </form>
+      </div>  
+    )
+  }
+}
+
+export default SignUp
