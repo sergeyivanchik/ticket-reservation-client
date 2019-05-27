2c0e8cb9102465e3e7e919abcbf197a1a3b873aa
sergeyivanchik <ivanchik-s@mail.ru>
2019-05-27 21:06:40 +0300

    rename selected-movie to movie

--- src/Components/Schedule/SelectedMovie/SelectedMovie.js
+++ src/Components/Schedule/SelectedMovie/SelectedMovie.js
@@ -7,18 +7,18 @@ class SelectedMovie extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div className="selected-movie" >
+      <div className="movie" >
         <img
           src={movie.poster}
           alt={movie.name}
-          className="selected-movie__poster"
+          className="movie__poster"
         >
         </img>
-        <div className="selected-movie__info">
-          <p className="selected-movie__title">
+        <div className="movie__info">
+          <p className="movie__title">
             {movie.name}
           </p>
-          <p className="selected-movie__description">
+          <p className="movie__description">
           {movie.description}
           </p>
         </div>
