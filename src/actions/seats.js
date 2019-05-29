5af91040eead94d6ba012618d544380c6f8b0c1a
sergeyivanchik <ivanchik-s@mail.ru>
2019-05-29 13:06:55 +0300

    add buying actions

--- src/actions/seats.js
+++ src/actions/seats.js
@@ -1,14 +1,39 @@
+import axios from "axios";
 import {
   SELECT_SEAT,
-  DELETE_SELECTED_SEATS
+  DELETE_SELECTED_SEATS,
+  BUY_SEATS_SUCCESS,
+  BUY_SEATS_FAILURE
 } from '../constants/seats.js';
 
 
 export const deleteSelectedSeats = () => ({
   type: DELETE_SELECTED_SEATS
-})
+});
 
 export const selectSeat = seat => ({
   type: SELECT_SEAT,
   payload: seat
 });
+
+export const buySeatsSuccess = seats => ({
+  type: BUY_SEATS_SUCCESS,
+  payload: seats
+});
+
+export const buySeatsFailure = error => ({
+  type: BUY_SEATS_FAILURE,
+  payload: error
+});
+
+export const buySeatsAsync = (sessionId, seats) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`http://localhost:8080/session/${sessionId}`, { seats });
+      dispatch(buySeatsSuccess(data))
+    }
+    catch (error) {
+      dispatch(buySeatsFailure(error));
+    }
+  }
+} 
