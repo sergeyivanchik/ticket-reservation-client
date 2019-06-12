0c473ada912aeacce5eb8373f769838cb511489d
sergeyivanchik <ivanchik-s@mail.ru>
2019-06-12 11:21:46 +0300

    add actions, consts, redusers for select , bought  and selected seats

--- src/actions/seats.js
+++ src/actions/seats.js
@@ -1,20 +1,88 @@
 import axios from "axios";
 import {
-  SELECT_SEAT,
+  SELECT_SEAT_SUCCESS,
+  SELECT_SEAT_FAILURE,
   DELETE_SELECTED_SEATS,
   BUY_SEATS_SUCCESS,
-  BUY_SEATS_FAILURE
+  BUY_SEATS_FAILURE,
+  GET_BOUGHT_SEATS_SUCCESS,
+  GET_BOUGHT_SEATS_FAILURE,
+  GET_SELECTED_SEATS_SUCCESS,
+  GET_SELECTED_SEATS_FAILURE
 } from '../constants/seats.js';
 
+
+export const getSelectedSeatsSuccess = selectedSeats => ({
+  type: GET_SELECTED_SEATS_SUCCESS,
+  payload: selectedSeats
+})
+
+export const getSelectedSeatsFailure = error => ({
+  type: GET_SELECTED_SEATS_FAILURE,
+  payload: error
+})
+
+export const getSelectedSeatsAsync = (sessionId, cinemaId, hallId, movieId, userId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`http://localhost:8080/selectedSeats/${userId}/${sessionId}/${cinemaId}/${hallId}/${movieId}`);
+      dispatch(getSelectedSeatsSuccess(data))
+    }
+    catch (error) {
+      dispatch(getSelectedSeatsFailure(error));
+    }
+  }
+}
+
+export const getBoughtSeatsSucces = boughtSeats => ({
+  type: GET_BOUGHT_SEATS_SUCCESS,
+  payload: boughtSeats
+})
+
+export const getBoughtSeatsFailure = error => ({
+  type: GET_BOUGHT_SEATS_FAILURE,
+  payload: error
+})
+
+export const getBoughtSeatsAsync = (sessionId, cinemaId, hallId, movieId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`http://localhost:8080/boughtSeats/${sessionId}/${cinemaId}/${hallId}/${movieId}`);
+      dispatch(getBoughtSeatsSucces(data))
+    }
+    catch (error) {
+      dispatch(getBoughtSeatsFailure(error));
+    }
+  }
+}
 
 export const deleteSelectedSeats = () => ({
   type: DELETE_SELECTED_SEATS
 });
 
-export const selectSeat = seat => ({
-  type: SELECT_SEAT,
+export const selectSeatSuccess = seat => ({
+  type: SELECT_SEAT_SUCCESS,
   payload: seat
 });
+
+export const selectSeatFailure = error => ({
+  type: SELECT_SEAT_FAILURE,
+  payload: error
+})
+
+export const selectSeatAsync = (user, session, cinema, hall, movie, row, seat, cost) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`http://localhost:8080/selectedSeats`, 
+        { user, session, cinema, hall, movie, row, seat, cost }
+      );
+      dispatch(selectSeatSuccess(data));
+    }
+    catch (error) {
+      dispatch(selectSeatFailure(error));
+    }
+  }
+}
 
 export const buySeatsSuccess = seats => ({
   type: BUY_SEATS_SUCCESS,
--- src/redusers/seats.js
+++ src/redusers/seats.js
@@ -1,14 +1,20 @@
 import {
-  SELECT_SEAT,
+  SELECT_SEAT_SUCCESS,
+  SELECT_SEAT_FAILURE,
   DELETE_SELECTED_SEATS,
   BUY_SEATS_SUCCESS,
-  BUY_SEATS_FAILURE
+  BUY_SEATS_FAILURE,
+  GET_BOUGHT_SEATS_SUCCESS,
+  GET_BOUGHT_SEATS_FAILURE,
+  GET_SELECTED_SEATS_SUCCESS,
+  GET_SELECTED_SEATS_FAILURE
 } from '../constants/seats.js';
 
 
 const initialState = {
-  selectSeats: [],
+  selectedSeats: [],
   reserveSeats: [],
+  boughtSeats: [],
   error: ''
 }
 
@@ -16,21 +22,32 @@ export default function seats(state = initialState, action) {
   switch (action.type) {
     case DELETE_SELECTED_SEATS:
       return Object.assign({}, state, {
-        selectSeats: []
+        selectedSeats: []
       })
 
-    case SELECT_SEAT: {
-      const currentSeat = state.selectSeats.find(seatInfo => 
+    case SELECT_SEAT_SUCCESS: {
+      const currentSeat = state.selectedSeats.find(seatInfo => 
         action.payload.row === seatInfo.row &&
         action.payload.seat === seatInfo.seat &&
-        action.payload.price === seatInfo.price
+        action.payload.cost === seatInfo.cost &&
+        action.payload.user === seatInfo.user &&
+        action.payload.session === seatInfo.session &&
+        action.payload.cinema === seatInfo.cinema &&
+        action.payload.hall === seatInfo.hall &&
+        action.payload.movie === seatInfo.movie
       )
-      const chooseSeat = currentSeat
-      ? state.selectSeats.filter(seat => seat !== currentSeat)
-      : [...state.selectSeats, action.payload]
+      const selectedSeat = currentSeat
+      ? state.selectedSeats.filter(seat => seat !== currentSeat)
+      : [...state.selectedSeats, action.payload]
 
       return Object.assign({}, state, {
-        selectSeats: chooseSeat
+        selectedSeats: selectedSeat
+      })
+    }
+
+    case SELECT_SEAT_FAILURE: {
+      return Object.assign({}, state, {
+        error: action.payload
       })
     }
 
@@ -40,7 +57,31 @@ export default function seats(state = initialState, action) {
       })
     }
 
+    case GET_SELECTED_SEATS_SUCCESS: {
+      return Object.assign({}, state, {
+        selectedSeats: action.payload
+      })
+    }
+
+    case GET_SELECTED_SEATS_FAILURE: {
+      return Object.assign({}, state, {
+        error: action.payload
+      })
+    }
+
     case BUY_SEATS_FAILURE: {
+      return Object.assign({}, state, {
+        error: action.payload
+      })
+    }
+
+    case GET_BOUGHT_SEATS_SUCCESS: {
+      return Object.assign({}, state, {
+        boughtSeats: action.payload
+      })
+    }
+
+    case GET_BOUGHT_SEATS_FAILURE: {
       return Object.assign({}, state, {
         error: action.payload
       })
