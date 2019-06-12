814f3e9ed50f5535f5a5f2f22f78ed9faaea10f0
sergeyivanchik <ivanchik-s@mail.ru>
2019-06-12 11:41:31 +0300

    remake login page

--- src/Components/Users/LogIn/LogIn.js
+++ src/Components/Users/LogIn/LogIn.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { logInAsync } from '../../../actions/users.js';
 
+import { logInAsync } from '../../../actions/users.js';
+import TopNavBar from '../../Navbars/TopNavbar/TopNavbar.js';
 import './LogIn.scss'
 
 
@@ -20,28 +21,31 @@ class LogIn extends React.Component {
 
   render() {
     return (
-      <div className="login">
-        <form className="login__form">
-          <label className="login__title">Log in</label>
-          <input
-            type="text"
-            name="username"
-            className="login__username"
-            placeholder="username"
-            onChange={this.handleInputChange}
-          />
-          <input
-            type="password"
-            name="password"
-            className="login__password"
-            placeholder="password"
-            onChange={this.handleInputChange}
-          />
-          <span className="login__button" onClick = {() => {this.props.onLogIn(this.state)}}>Log in</span>
-          <Link to="/signup" className='login__link-to'>
-            <span className="login__signup">Sign up</span>
-          </Link>
-        </form>
+      <div>
+      <TopNavBar/>
+        <div className="login">
+          <form className="login__form">
+            <label className="login__title">Log in</label>
+            <input
+              type="text"
+              name="username"
+              className="login__username"
+              placeholder="username"
+              onChange={this.handleInputChange}
+            />
+            <input
+              type="password"
+              name="password"
+              className="login__password"
+              placeholder="password"
+              onChange={this.handleInputChange}
+            />
+            <span className="login__button" onClick = {() => {this.props.onLogIn(this.state)}}>Log in</span>
+            <Link to="/signup" className='login__link-to'>
+              <span className="login__signup">Sign up</span>
+            </Link>
+          </form>
+        </div>
       </div>
     )
   }
